d4206ce73ed800a927f930bc2a69711ccb62699b
Armida-Contreras <128248644+Armida-Contreras@users.noreply.github.com>
2023-04-01 20:57:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,37 +14,36 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 let posts = []; //global variable
 
-app.get("/", function(req, res) { //callback
+app.get("", function(req, res) { //callback
   res.render("home", {
     startingContent: homeStartingContent, 
     posts: posts
   });
 });
 
-app.get("/about", function(req, res) {
+app.get("about", function(req, res) {
   res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req, res) {
+app.get("contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res) {
+app.get("compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
+app.post("compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postPublish
   };
   posts.push(post); //push to add element into array
 
-  res.redirect("/");
+  res.redirect("");
 });
 
 app.get("/posts/:postName", function(req, res) {
